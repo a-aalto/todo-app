292cf6ca477d5874e1b32c5dbb26e7860775c108
Antti Aalto <antti.johannes.aalto@student.laurea.fi>
2025-10-20 13:19:38 +0300

    Refactoring of the javascript code. Structural changes to the list elements the user creates. Checkbox added to the tasks.

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,77 @@
+let numberOfTasks = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
-    let form = document.getElementById("inputForm");
-    form.addEventListener("submit", getInput);
+    const form = document.getElementById("inputForm");
+    form.addEventListener("submit", addNewTask);
 });
 
-function getInput(event) {
+function addNewTask(event) {
     event.preventDefault();
 
-    let input = document.getElementById("input");
-    let taskList = document.getElementById("task-list");
-    let text = input.value.trim();
+    // create variables for the various elements
+    const input = document.getElementById("input");
+    const taskList = document.getElementById("task-list");
+    const text = input.value.trim();
+    const counterElement = document.getElementById("counter");
+    const listFooter = document.getElementById("list-footer");
 
     if (text !== "") {
-        let newTask = document.createElement("li");
-        newTask.textContent = text;
-        newTask.classList.add("list-group-item", "text-start");
-        taskList.appendChild(newTask);
+
+        // create task element with the user input
+        const newListItem = createTaskElement(text);
+
+        // add the task to the list-element ("ul")
+        addTaskToList(taskList,newListItem);
+
+        // increment the number of tasks by one when user has created a task
+        numberOfTasks += 1;
+
+        // add the number of tasks to the element displaying the total number of tasks
+        counterElement.innerHTML = numberOfTasks;
+        updateTaskFooterVisibility(numberOfTasks, listFooter);
+
+        // reset input field
         input.value = "";
     }
+}
 
+function createTaskElement(text) {
+    // create elements for the task
+    const newListElement = document.createElement("li");
+    const newDiv = document.createElement("div");
+    const newInputButton = document.createElement("input");
+    const newLabel = document.createElement("label");
 
+    // add css and types to elements
+    newListElement.classList.add("list-group-item", "text-start");
+    newDiv.classList.add("form-check");
+    newInputButton.classList.add("me-3", "form-check-input");
+    newLabel.classList.add("form-check-label");
+    newInputButton.type = "checkbox";
+
+    // add user input to the task element
+    newLabel.textContent = text;
+
+    // append elements structurally
+    newListElement.appendChild(newDiv);
+    newDiv.appendChild(newInputButton);
+    newDiv.appendChild(newLabel);
+
+    return newListElement;
+
+}
+function addTaskToList(taskList, newListElement) {
+
+    // add the newListElement ("li") to the tasklist ("ul")
+    taskList.appendChild(newListElement);
+}
+function updateTaskFooterVisibility(numberOfTasks, listFooter) {
+
+    // update the visibility of listfooter-element depending if there's tasks on the list or not
+    if (numberOfTasks > 0) {
+        listFooter.classList.remove("d-none");
+    }
+    else {
+        listFooter.classList.add("d-none");
+    }
 }
